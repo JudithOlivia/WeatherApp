2db20f5a794da60986fe40daa9788e3e83592ef1
JudithOlivia <jxdith13@gmail.com>
2026-02-22 02:19:19 -0500

    add base url

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const API_KEY = "YOUR_API_KEY_HERE"; 
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const cityInput = document.getElementById("cityInput");
 const searchButton = document.getElementById("searchButton");
@@ -7,5 +8,18 @@ const weatherInfo = document.getElementById("weatherInfo");
 searchButton.addEventListener('click', handleSearch);
 cityInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') handleSearch();
-})
+});
 
+async function handleSearch() {
+    const city = cityInput.ariaValueMax.trim();
+
+    if (!city) {
+        showError('Please enter a city name.');
+        return;
+    }
+    await fetchWeatherData(city);
+}
+
+async function fetchWeatherData(city) {
+
+}
